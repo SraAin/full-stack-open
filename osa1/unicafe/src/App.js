918f432e78897116a824a66172f55c2851961575
SraAin <sara.ainali@gmail.com>
2023-02-13 16:36:19 +0200

    unicafe, step1 updated

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,14 +1,42 @@
 import { useState } from 'react'
 
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const addGoodClick = () => setGood(good + 1)
+
+  const addNeutralClick = () => setNeutral(neutral + 1)
+
+  const addBadClick = () => setBad(bad + 1)
+
   return (
     <div>
-      code here
+      <h1>Give Feedback</h1>
+      <Button handeClick={addGoodClick} title='Good' />
+      <Button handeClick={addNeutralClick} title='Neutral' />
+      <Button handeClick={addBadClick} title='Bad' />
+      <Statistics goodClicks={good} neutralClicks={neutral} badClicks={bad} />
+    </div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <div>
+      <button onClick={props.handeClick}>{props.title}</button>
+    </div>
+  )
+}
+
+const Statistics = (props) => {
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>Good: {props.goodClicks} </p>
+      <p>Neutral: {props.neutralClicks} </p>
+      <p>Bad: {props.badClicks} </p>
     </div>
   )
 }
