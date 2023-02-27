8a3e94948812859c86d5ea34b6823a90cf24aff8
SraAin <sara.ainali@gmail.com>
2023-02-27 14:51:33 +0200

    puhelinluettelo, step3

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,14 +1,16 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '0401234123' }]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const addPerson = (event) => {
     event.preventDefault();
     // console.log('Button clicked', event.target)
     const newObj = {
       name: newName,
+      number: newNumber
     };
 
     // Ehdollinen operaattori, jolla varmistetaan, että uutta nimeä ei löydy puhelinluettelosta
@@ -17,12 +19,17 @@ const App = () => {
       : setPersons(persons.concat(newObj));
 
     setNewName('');
+    setNewNumber('');
   };
 
   const handleNameChange = (event) => {
     // console.log(event.target.value);
     setNewName(event.target.value);
   };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  }
 
   return (
     <div>
@@ -32,12 +39,15 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <p key={person.name}>{person.name}</p>
+        <p key={person.name}>{person.name} {person.number}</p>
       ))}
     </div>
   );
