5f2934847b7c848f82f0ace4cecdc347ad2a89fd
SraAin <sara.ainali@gmail.com>
2023-02-28 11:07:00 +0200

    puhelinluettelo, step4

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,16 +1,22 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '0401234123' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '0401234123' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' },
+  ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
 
   const addPerson = (event) => {
     event.preventDefault();
     // console.log('Button clicked', event.target)
     const newObj = {
       name: newName,
-      number: newNumber
+      number: newNumber,
     };
 
     // Ehdollinen operaattori, jolla varmistetaan, että uutta nimeä ei löydy puhelinluettelosta
@@ -22,6 +28,13 @@ const App = () => {
     setNewNumber('');
   };
 
+  const searchNames =
+    filter === ''
+      ? persons
+      : persons.filter((person) =>
+          person.name.toLocaleLowerCase().includes(filter)
+        );
+
   const handleNameChange = (event) => {
     // console.log(event.target.value);
     setNewName(event.target.value);
@@ -29,11 +42,19 @@ const App = () => {
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
-  }
+  };
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown with <input value={filter} onChange={handleFilterChange} />
+      </div>
+      <h2>Add</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -46,8 +67,10 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <p key={person.name}>{person.name} {person.number}</p>
+      {searchNames.map((person) => (
+        <p key={person.name}>
+          {person.name} {person.number}
+        </p>
       ))}
     </div>
   );
