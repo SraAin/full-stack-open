49fd6cfeed8d29b79acf7061df5c65e8a06397db
SraAin <sara.ainali@gmail.com>
2023-03-23 11:16:30 +0200

    osa 3, puhelinluettelo, step 1,2,3,4

---
+++ osa3/puhelinluettelo/index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+
+let persons = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendick',
+    number: '39-23-6423857',
+  },
+];
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const person = persons.find(p => p.id === id);
+
+    if (person) {
+        response.json(person);
+    } else {
+        response.status(404).end();
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end();
+})
+
+app.get('/info', (request, response) => {
+    const date = new Date();
+    console.log(date);
+
+    response.end(
+      `Phonebook has info for ${persons.length} people 
+
+${date}`
+    );
+})
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
