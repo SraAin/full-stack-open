09b5486b66e4a0ab656850489700c7095551cee5
SraAin <sara.ainali@gmail.com>
2023-04-14 12:57:24 +0300

    tehtävä 3.17

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -32,17 +32,17 @@ let persons = [
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message);
-  
+
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
+    return response.status(400).send({ error: 'malformatted id' });
   }
 
   next(error);
-}
+};
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: 'unknown endpoint' });
+};
 
 app.use(express.json());
 app.use(
@@ -60,15 +60,19 @@ app.get('/api/persons', (request, response) => {
 });
 
 app.get('/api/persons/:id', (request, response, next) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  }).catch(error => next(error));
+  Person.findById(request.params.id)
+    .then((person) => {
+      response.json(person);
+    })
+    .catch((error) => next(error));
 });
 
 app.delete('/api/persons/:id', (request, response, next) => {
-  Person.findByIdAndRemove(request.params.id).then((person) => {
-    response.status(204).end();
-  }).catch(error => next(error));
+  Person.findByIdAndRemove(request.params.id)
+    .then((person) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 /*const generateId = () => {
@@ -96,9 +100,24 @@ app.post('/api/persons', (request, response, next) => {
     .then((savedPerson) => {
       console.log(`added ${person.name} number ${person.number} to phonebook`);
       response.json(savedPerson);
-    }).catch(error => next(error));
+    })
+    .catch((error) => next(error));
 });
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
 
 /*app.post('/api/persons', (request, response) => {
   const body = request.body;
