91a4f0261d26fd229cb74b962fe8507284780000
SraAin <sara.ainali@gmail.com>
2023-04-19 11:01:51 +0300

    4.4 apufunktioita ja yksikkötestejä, step2

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  return blogs.length === 0
+  ? 0
+  : blogs.reduce((total, currentVal) => total + currentVal.likes, 0)
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 };
