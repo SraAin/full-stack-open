f6dbb8530fad7cd32bf1a16c3da0743d7526777b
SraAin <sara.ainali@gmail.com>
2023-04-19 11:34:05 +0300

    4.5 apufunktioita ja yksikkötestejä, step3

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -4,11 +4,19 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => {
   return blogs.length === 0
-  ? 0
-  : blogs.reduce((total, currentVal) => total + currentVal.likes, 0)
-}
+    ? 0
+    : blogs.reduce((total, currentVal) => total + currentVal.likes, 0);
+};
+
+// favoriteBlog-funktio selvittää millä blogilla on eniten tykkäyksiä
+const favoriteBlog = (blogs) => {
+  const highestLikeAmount = Math.max(...blogs.map((blog) => blog.likes));
+
+  return blogs.find((blog) => blog.likes === highestLikeAmount);
+};
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
