c0341f8232771ad2c08a30fb1b648f119e8520a2
SraAin <sara.ainali@gmail.com>
2023-07-18 17:23:10 +0300

    5.7 blogilistan frontend, step7

--- osa5/blogilista-frontend/src/components/Blog.js
+++ osa5/blogilista-frontend/src/components/Blog.js
@@ -1,7 +1,40 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} BY {blog.author}
-  </div>  
-)
+import { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [blogInfoVisible, setBlogInfoVisible] = useState(false);
+
+  const showWhenBlogVisible = { display: blogInfoVisible ? '' : 'none' };
+  const hideWhenBlogVisible = { display: blogInfoVisible ? 'none' : '' };
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingBottom: 10,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div>
+      <div style={hideWhenBlogVisible}>
+        <div style={blogStyle}>
+        {blog.title} BY {blog.author}
+        <button onClick={() => setBlogInfoVisible(true)}>View</button>
+        </div>
+      </div>
+      <div style={showWhenBlogVisible}>
+        <div style={blogStyle}>
+        <p>
+          {blog.title} BY {blog.author}{' '}
+          <button onClick={() => setBlogInfoVisible(false)}>Hide</button>
+        </p>
+        <p>{blog.url}</p>
+        <p>Likes: {blog.likes}</p>
+        <p>{blog.user.name}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
